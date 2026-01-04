f16839095ea657cfab03542c60b2c1d20be3dc2f
Pavel <paulavetspavel@outlook.com>
2026-01-04 18:25:46 +0300

    Fix server.js

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
-    console.log('Start test');
-    console.log('Test');
   });
 }
 
