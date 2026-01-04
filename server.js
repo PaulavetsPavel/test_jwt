2d1692bd7c243f54c8788d1625c8fd5326e15113
Pavel <paulavetspavel@outlook.com>
2026-01-04 18:19:11 +0300

    New commit

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
+    console.log('Start test');
   });
 }
 
